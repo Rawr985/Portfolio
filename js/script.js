21a71239ccb06d2b2e835ab48dfbebe1b40ac96b
Rawr985 <130183796+Rawr985@users.noreply.github.com>
2024-06-13 21:49:37 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+const sections = document.querySelectorAll('section[data-background]');
+
+window.addEventListener('scroll', () => {
+  const currentSection = getCurrentSection();
+  if (currentSection) {
+    const backgroundImage = currentSection.getAttribute('data-background');
+    document.body.style.backgroundImage = `url('images/${backgroundImage}')`;
+    document.body.style.transition = 'background-image 0.5s ease-in-out';
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'center';
+  }
+});
+
+function getCurrentSection() {
+  const scrollPosition = window.scrollY + window.innerHeight / 2;
+  for (const section of sections) {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.offsetHeight;
+    if (scrollPosition >= sectionTop && scrollPosition < sectionTop + sectionHeight) {
+      return section;
+    }
+  }
+  return null;
+}
+
 function openImage(imageUrl) {
-    window.open(imageUrl, '_blank', 'fullscreen=yes');
+  window.open(imageUrl, '_blank', 'fullscreen=yes');
 }
